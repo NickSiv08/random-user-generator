9fa6ee38bbaf9ab5f2fb3374b673be7773c2e70d
giannos75 <giasivros@gmail.com>
2023-04-30 21:01:05 +0300

    Final Commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const button = document.getElementById('generate')
+const user = document.getElementById('user')
+
+function generateUser() {
+  fetch('https://randomuser.me/api/')
+    .then((response) => response.json())
+    .then((data) => {
+      const fetchedUser = data.results[0]
+      user.innerHTML = `
+        <div class="flex justify-between">
+          <div class="flex">
+            <img
+              class="w-48 h-48 rounded-full mr-8"
+              src="${fetchedUser.picture.large}"
+            />
+            <div class="space-y-3">
+              <p class="text-xl">
+                <span class="font-bold">Name: </span>${fetchedUser.name.first} ${fetchedUser.name.last}
+              </p>
+              <p class="text-xl">
+                <span class="font-bold">Email: </span> ${fetchedUser.email}
+              </p>
+              <p class="text-xl">
+                <span class="font-bold">Phone: </span> ${fetchedUser.phone}
+              </p>
+              <p class="text-xl">
+                <span class="font-bold">Location: </span> ${fetchedUser.location.city} ${fetchedUser.location.state}
+              </p>
+              <p class="text-xl"><span class="font-bold">Age: </span> ${fetchedUser.dob.age}</p>
+            </div>
+          </div>
+        </div>
+        `
+      if (fetchedUser.gender === 'female') {
+        document.body.classList.add('bg-purple-800')
+      } else {
+        document.body.classList.add('bg-blue-800')
+      }
+    })
+}
+
+button.addEventListener('click', generateUser)
